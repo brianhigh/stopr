9b9475b2ce362f496d66074162765a53a3448d96
Brian High <high@uw.edu>
2019-11-01 12:54:23 -0700

    Added input validation for parameters.

--- R/find_stops.R
+++ R/find_stops.R
@@ -3,11 +3,11 @@
 #' Find stops made in a route given coordinates and timestamps from a GPS track.
 #' @param x (data.frame) GPS dataset.
 #' @param stop_min_duration_s (integer) Minimum stop duration cutoff in seconds. 
-#'     (Default: 10)
+#'     (Must be greater than k. Default: 10)
 #' @param digits (integer) Decimal places used for rounding. 
-#'     (Default: 3)
+#'     (Must be 0 or greater. Default: 3)
 #' @param k (integer) Window length for rolling median. 
-#'     (Must be odd. Default: 3)
+#'     (Must be odd, 3 or greater. Default: 3)
 #' @return (tibble) The coordinates and timestamps of the identified stops, 
 #'     along with the stop durations (integer), will be returned as a tibble.
 #' @keywords GPS, stops
@@ -31,8 +31,17 @@
 #' find_stops(df)
 #' }
 find_stops <- function(x, stop_min_duration_s = 10, digits = 3, k = 3) {
+  # Define expected variables for data.frame 'x'.
   vars <- c('datetime', 'latitude', 'longitude')
-  if ("data.frame" %in% class(x) & identical(vars, intersect(vars, names(x)))) {
+  
+  # Convert other parameters to expected data types.
+  stop_min_duration_s <- as.integer(stop_min_duration_s)
+  digits <- as.integer(digits)
+  k <- as.integer(k)
+  
+  # Find stops in x if expected variables are present and parameters are valid.
+  if ("data.frame" %in% class(x) & identical(vars, intersect(vars, names(x))) &
+      stop_min_duration_s > k & digits >= 0 & k >= 3 & k %% 2 == 1) {
     x %>% dplyr::mutate(latitude = as.numeric(latitude),
                         longitude = as.numeric(longitude),
                         datetime = lubridate::as_datetime(datetime)) %>%
@@ -46,5 +55,7 @@ find_stops <- function(x, stop_min_duration_s = 10, digits = 3, k = 3) {
       dplyr::filter(duration >= stop_min_duration_s) %>% 
       dplyr::select(-stopid) %>% 
       tidyr::drop_na()
+  } else {
+    stop("Input parameters are missing or invalid. See: ?stopr::find_stops")
   }
 }
