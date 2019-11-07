5afd624a9deb816ccd0d5f6027e1204c8a8cb34b
Brian High <high@uw.edu>
2019-11-07 15:59:03 -0800

    Added a test for new function parameters with raw GPX file.

--- tests/testthat/test-find_stops_test.R
+++ tests/testthat/test-find_stops_test.R
@@ -7,3 +7,15 @@ test_that("find_stops works", {
   # testthat::compare(stops, stops_ref)
   expect_equal(stops, stops_ref)
 })
+library(tibble)
+library(plotKML)
+test_that("find_stops works with raw gpx file", {
+  data_dir <- file.path("..", "..", "inst", "extdata")
+  stops <- find_stops(
+    as_tibble(readGPX(file.path(data_dir, "test_data.gpx"))$tracks[[1]][[1]]), 
+    stop_min_duration_s = 20, k = 5, digits = 3, 
+    .vars = c(lon = 'lon', lat = 'lat', time = 'time'))
+  stops_ref <- read_csv(file.path(data_dir, "stops.csv"))
+  # testthat::compare(stops, stops_ref)
+  expect_equal(stops, stops_ref)
+})
