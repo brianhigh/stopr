c13d5a996cb1c75282d206d970011fd5d1e2130c
Brian High <high@uw.edu>
2019-11-07 11:36:47 -0800

    Used .data and .vars variable names, consitent with some tidyverse functions.

--- R/find_stops.R
+++ R/find_stops.R
@@ -1,7 +1,7 @@
 #' Find Stops
 #'
 #' Find stops made in a route given coordinates and timestamps from a GPS track.
-#' @param x (data.frame) GPS dataset.
+#' @param .data (data.frame) GPS dataset.
 #' @param stop_min_duration_s (integer) Minimum stop duration cutoff in seconds. 
 #'     (Must be greater than k. Default: 10)
 #' @param digits (integer) Decimal places used for rounding. 
@@ -30,9 +30,9 @@
 #' df <- read_csv(system.file("extdata", "test_data.csv", package = "stopr"))
 #' find_stops(df)
 #' }
-find_stops <- function(x, stop_min_duration_s = 10, digits = 3, k = 3) {
-  # Define expected variables for data.frame 'x'.
-  vars <- c('datetime', 'latitude', 'longitude')
+find_stops <- function(.data, stop_min_duration_s = 10, digits = 3, k = 3) {
+  # Define expected variables for data.frame '.data'.
+  .vars <- c('datetime', 'latitude', 'longitude')
   
   # Convert other parameters to expected data types.
   stop_min_duration_s <- as.integer(stop_min_duration_s)
@@ -40,11 +40,11 @@ find_stops <- function(x, stop_min_duration_s = 10, digits = 3, k = 3) {
   k <- as.integer(k)
   
   # Find stops in x if expected variables are present and parameters are valid.
-  if (is.data.frame(x) & identical(vars, intersect(vars, names(x))) &
+  if (is.data.frame(.data) & identical(.vars, intersect(.vars, names(.data))) &
       stop_min_duration_s > k & digits >= 0 & k >= 3 & k %% 2 == 1) {
-    x %>% dplyr::mutate(latitude = as.numeric(latitude),
-                        longitude = as.numeric(longitude),
-                        datetime = lubridate::as_datetime(datetime)) %>%
+    .data %>% dplyr::mutate(latitude = as.numeric(latitude),
+                            longitude = as.numeric(longitude),
+                            datetime = lubridate::as_datetime(datetime)) %>%
       dplyr::mutate_at(.vars = dplyr::vars(latitude, longitude),
             .funs = list(rnd = ~round(zoo::rollmedianr(., k, NA), digits))) %>%
       dplyr::mutate(loc = paste(latitude_rnd, longitude_rnd, sep = ',')) %>%
